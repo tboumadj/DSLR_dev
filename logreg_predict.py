4e7a22f6173d2f7fdee514cd13d656f04625bba3
Theo Boumadjer <tboumadj@k2r1p9.42mulhouse.fr>
2026-05-21 14:25:11 +0200

    Prep-Loader

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,6 +1,10 @@
 import sys
 
 def main():
+#--------Loader
+    filepath = './datasets/dataset_test.csv'
+
+#----------------
     print("\033[33m### Run Predict Part ... ####\033[0m")
 
 if __name__ == '__main__':
--- logreg_train.py
+++ logreg_train.py
@@ -1,6 +1,10 @@
 import sys
 
 def main():
+#--------Loader
+    filepath = './datasets/dataset_train.csv'
+
+#---------------
     print("\033[33m### Run Training Part ... ####\033[0m")
 
 if __name__ == '__main__':
