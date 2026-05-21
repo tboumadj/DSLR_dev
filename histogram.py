5018ad9e8626a2a1bdf839a48fcc4d4db9d10f37
Theo Boumadjer <tboumadj@k2r1p9.42mulhouse.fr>
2026-05-21 10:23:59 +0200

    add/histogram+FixGitignore

---
+++ histogram.py
@@ -0,0 +1,23 @@
+import sys
+from utils.load import load_csv, get_hist_house
+from utils.stats import print_graph_hist
+
+def main():
+    # if len(sys.argv) != 4:
+    #     print("\033[33m#### ---> Python histogram.py datasets/dataset_train.csv Feature ####\033[0m")
+    #     sys.exit(1)
+
+    #-------- Loader
+    #filepath = sys.argv[1]
+    filepath = './datasets/dataset_train.csv'
+    dataset = load_csv(filepath)
+
+    #feat = sys.argv[2]
+    feat = "Herbology"
+    #---------------
+
+    data_house = get_hist_house(filepath, dataset, feat)
+    print_graph_hist(data_house, feat)
+
+if __name__ == '__main__':
+    main()
