134e50c4cf9d1c4d69d2216a07699159a29528fb
tboumadj <thenbouma@gmail.com>
2026-05-22 15:10:10 +0200

    PrepData/ClearTest

--- logreg_train.py
+++ logreg_train.py
@@ -28,18 +28,7 @@ def main():
     X, y = extract_X_y(dataframe_norm, valid_feat)
  
 #------------------Test Print
-    print(f"Élèves             : {len(X)}")
-    print(f"Features           : {len(X[0])}")
-    print(f"Maisons uniques    : {set(y)}")
-    print(f"Premier élève      : {[round(v, 4) for v in X[0]]}")
-    print(f"Sa maison          : {y[0]}")
 
-    print(f"\nVérification normalisation :")
-    for i, feat in enumerate(valid_feat):
-        col  = [X[j][i] for j in range(len(X))]
-        mean = sum(col) / len(col)
-        std  = (sum((x - mean) ** 2 for x in col) / len(col)) ** 0.5
-        print(f"  {feat:<35}  mean={mean:>8.5f}  std={std:>6.4f}")
 #---------------
     print("\033[33m### Generate JsonConfig for predict ... ####\033[0m")
 
