b36034879e551ca98ef5d26b8753dc8c2c51a793
tboumadj <thenbouma@gmail.com>
2026-05-22 12:10:25 +0200

    Prep+testNormalize

--- logreg_train.py
+++ logreg_train.py
@@ -1,5 +1,6 @@
 import sys
 from utils.load import is_numeric_column, load_csv, dataset_to_dataframe
+from utils.train import normalize_feat, extract_X_y
 
 def main():
 #--------Loader
@@ -18,10 +19,27 @@ def main():
     ]
     
     dataframe = dataset_to_dataframe(filepath, valid_feat)
-    print(f'valid dataframe : {dataframe}')
+    print(f'valid dataframe : {dataframe.shape}')
 
 #--------Noralisation
 
+    dataframe_norm, params = normalize_feat(dataframe, valid_feat)
+    print(f'df_norm : {dataframe_norm.shape}')
+    X, y = extract_X_y(dataframe_norm, valid_feat)
+ 
+#------------------Test Print
+    print(f"Élèves             : {len(X)}")
+    print(f"Features           : {len(X[0])}")
+    print(f"Maisons uniques    : {set(y)}")
+    print(f"Premier élève      : {[round(v, 4) for v in X[0]]}")
+    print(f"Sa maison          : {y[0]}")
+
+    print(f"\nVérification normalisation :")
+    for i, feat in enumerate(valid_feat):
+        col  = [X[j][i] for j in range(len(X))]
+        mean = sum(col) / len(col)
+        std  = (sum((x - mean) ** 2 for x in col) / len(col)) ** 0.5
+        print(f"  {feat:<35}  mean={mean:>8.5f}  std={std:>6.4f}")
 #---------------
     print("\033[33m### Generate JsonConfig for predict ... ####\033[0m")
 
--- utils/train.py
+++ utils/train.py
@@ -1,4 +1,20 @@
 import json
 
+def normalize_feat(df, valid_feat):
+    params = {}
+    df_norm = df.copy()
+
+    for col in valid_feat:
+        mean = df[col].mean()
+        std  = df[col].std()
+        params[col] = {'mean': mean, 'std': std}
+        df_norm[col] = (df[col] - mean) / std if std != 0 else 0.0
+    return df_norm, params
+
+def extract_X_y(df, valid_feat):
+    X = df[valid_feat].values.tolist()
+    y = df['Hogwarts House'].tolist()
+    return X, y
+
 def save_config(house):
     return
