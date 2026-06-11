81f53d89daa766653a7385b95366b3296fb41b49
tboumadj <thenbouma@gmail.com>
2026-06-11 12:14:52 +0200

    V1_vs_V2

--- logreg_predict.py
+++ logreg_predict.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np
 
-from utils.load import dataset_to_dataframe_v2
+from utils.load import dataset_to_dataframe_v2, dataset_to_dataframe
 from utils.train import extract_X_y, standardize_feat
 from utils.test.test import evaluate_accuracy
 
@@ -27,7 +27,8 @@ def main():
         print("weights.json not found, run logreg_train.py first")
         sys.exit(1)
 
-    dataframe = dataset_to_dataframe_v2(FILEPATH, features, keep_houses=False)
+    dataframe = dataset_to_dataframe(FILEPATH, features, keep_houses=False)
+    # dataframe = dataset_to_dataframe_v2(FILEPATH, features, keep_houses=False)
     dataframe_stand, params = standardize_feat(dataframe, features)
 
     X = np.hstack([np.ones((dataframe_stand.shape[0], 1)), dataframe_stand])
